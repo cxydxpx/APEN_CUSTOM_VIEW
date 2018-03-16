ac5d2e2ea7ef302fafa98c9467920349aca09438
cxydxpx <578479132@qq.com>
2018-03-16 21:32:42 +0800

    自定义 七日年化图表 start

---
+++ app/src/main/java/com/example/apen/apencustomview/view/CurveView.java
@@ -0,0 +1,33 @@
+package com.example.apen.apencustomview.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 作者 Y_MS
+ * Created by ${APEN} on 2018-03-16.
+ * GitHub：https://github.com/cxydxpx
+ */
+
+public class CurveView extends View {
+
+    public CurveView(Context context) {
+        super(context);
+        initView();
+    }
+
+    public CurveView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CurveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void initView() {
+
+    }
+
+}
